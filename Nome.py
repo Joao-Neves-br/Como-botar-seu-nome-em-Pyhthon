615b98190d59f84fe8654c53066beebb7ab45924
João Pedro Neves de Azevedo Melo <joaopedronevesdeazevedomelo@gmail.com>
2024-10-24 11:53:52 -0300

    Add files via upload

---
+++ Nome.py
@@ -0,0 +1,4 @@
+#Como colocar seu nome completo no Python
+nome=input("Insira o seu nome:\n")
+sobrenome=input("Insira o seu sobrenome:\n")
+print("Seja Bem-vindo(a), {} {}!".format(nome,sobrenome))
